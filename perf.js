6a0b4c0aac78bd663e2ced327a66576b68706391
Alizain Feerasta <alizain.feerasta@gmail.com>
2016-07-30 18:48:57 -0400

    Add more performance instrumentation

--- perf.js
+++ perf.js
@@ -1,7 +1,19 @@
-var ulid = require('./index.js')
+var ulid = require('./')
 
 suite('ulid', function() {
+
+  set('iterations', 100000);
+
+  bench('encodeTime', function() {
+    ulid.encodeTime()
+  })
+
+  bench('encodeRandom', function() {
+    ulid.encodeRandom()
+  })
+
   bench('generate', function() {
     ulid()
   })
+
 })
