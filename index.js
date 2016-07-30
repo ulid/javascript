c9f10c0c9d214b40352a2ab0b54e31ff3c9d6906
Alizain Feerasta <alizain.feerasta@gmail.com>
2016-07-30 18:58:33 -0400

    Add global closure and proper exports for node/broswer environments

--- index.js
+++ index.js
@@ -1,40 +1,59 @@
-var crypto = require('crypto')
+(function(_window) {
 
-// Crockford's Base32
-// https://en.wikipedia.org/wiki/Base32
-var ENCODING = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+  'use strict';
 
-function strongRandomNumber() {
-  return crypto.randomBytes(4).readUInt32LE() / 0xFFFFFFFF
-}
+  var crypto = require('crypto')
 
-function encodeTime(now, len) {
-  var mod
-  var now
-  var str = ''
-  for (var x = len; x > 0; x--) {
-    mod = now % ENCODING.length
-    str = ENCODING.charAt(mod) + str
-    now = (now - mod) / ENCODING.length
+  // Crockford's Base32
+  // https://en.wikipedia.org/wiki/Base32
+  var ENCODING = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+  var ENCODING_LEN = ENCODING.length
+
+  function strongRandomNumber() {
+    return crypto.randomBytes(4).readUInt32LE() / 0xFFFFFFFF
   }
-  return str
-}
 
-function encodeRandom(len) {
-  var rand
-  var str = ''
-  for (var x = 0; x < len; x++) {
-    rand = Math.floor(ENCODING.length * strongRandomNumber())
-    str = ENCODING.charAt(rand) + str
+  function encodeTime(now, len) {
+    var mod
+    var now
+    var str = ''
+    for (var x = len; x > 0; x--) {
+      mod = now % ENCODING_LEN
+      str = ENCODING.charAt(mod) + str
+      now = (now - mod) / ENCODING_LEN
+    }
+    return str
   }
-  return str
-}
 
-function ulid() {
-  return encodeTime(Date.now(), 10) + encodeRandom(16)
-}
+  function encodeRandom(len) {
+    var rand
+    var str = ''
+    for (var x = 0; x < len; x++) {
+      rand = Math.floor(ENCODING_LEN * strongRandomNumber())
+      str = ENCODING.charAt(rand) + str
+    }
+    return str
+  }
 
-ulid.strongRandomNumber = strongRandomNumber
-ulid.encodeTime = encodeTime
-ulid.encodeRandom = encodeRandom
-module.exports = ulid
+  function ulid() {
+    return encodeTime(Date.now(), 10) + encodeRandom(16)
+  }
+
+  ulid.strongRandomNumber = strongRandomNumber
+  ulid.encodeTime = encodeTime
+  ulid.encodeRandom = encodeRandom
+
+  if (('undefined' !== typeof module) && module.exports) {
+    module.exports = ulid
+  }
+  else if (typeof define === 'function' && define.amd) {
+    define(function() {
+      return ulid;
+    })
+  }
+  else {
+    _window.ulid = ulid
+  }
+
+
+})('undefined' !== typeof window ? window : null)
