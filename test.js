ba27e2e3099e41273113dde0c23ab4f18cc07d27
Alizain Feerasta <alizain.feerasta@gmail.com>
2017-10-23 20:51:19 -0400

    Remove es5 features from test file

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
-const assert = require('assert')
-const lolex = require('lolex')
-const ulid = require('./')
+var assert = require('assert')
+var lolex = require('lolex')
+var ulid = require('./')
 
-describe('ulid', () => {
+describe('ulid', function() {
 
-  describe('prng', () => {
+  describe('prng', function() {
 
-    it('should produce a number', () => {
+    it('should produce a number', function() {
       assert.strictEqual(false, isNaN(ulid.prng()))
     })
 
-    it('should be between 0 and 1, tested many times', () => {
+    it('should be between 0 and 1, tested many times', function() {
       for (var x = 0; x < 1000; x++) {
         var num = ulid.prng()
         assert(num >= 0 && num <= 1)
@@ -19,31 +19,31 @@ describe('ulid', () => {
 
   })
 
-  describe('incremenet base32', () => {
+  describe('incremenet base32', function() {
 
-    it('increments correctly', () => {
+    it('increments correctly', function() {
       assert.strictEqual('A109D', ulid.incrementBase32('A109C'))
     })
 
-    it('carries correctly', () => {
+    it('carries correctly', function() {
       assert.strictEqual('A1Z00', ulid.incrementBase32('A1YZZ'))
     })
 
-    it('double increments correctly', () => {
+    it('double increments correctly', function() {
       assert.strictEqual('A1Z01', ulid.incrementBase32(ulid.incrementBase32('A1YZZ')))
     })
 
-    it('throws when it cannot increment', () => {
-      assert.throws(() => ulid.incrementBase32('ZZZ'))
+    it('throws when it cannot increment', function() {
+      assert.throws(function() ulid.incrementBase32('ZZZ'))
     })
 
   })
 
-  describe('randomChar', () => {
+  describe('randomChar', function() {
 
-    const sample = {}
+    var sample = {}
 
-    for (let x = 0; x < 100000; x++) {
+    for (var x = 0; x < 100000; x++) {
       char = String(ulid.randomChar()) // for if it were to ever return undefined
       if (sample[char] === undefined) {
         sample[char] = 0
@@ -51,136 +51,135 @@ describe('ulid', () => {
       sample[char] += 1
     }
 
-    it('should never return undefined', () => {
+    it('should never return undefined', function() {
       assert.strictEqual(undefined, sample['undefined'])
     })
 
-    it('should never return an empty string', () => {
+    it('should never return an empty string', function() {
       assert.strictEqual(undefined, sample[''])
     })
 
   })
 
-  describe('encodeTime', () => {
+  describe('encodeTime', function() {
 
-    it('should return expected encoded result', () => {
+    it('should return expected encoded result', function() {
       assert.strictEqual('01ARYZ6S41', ulid.encodeTime(1469918176385, 10))
     })
 
-    it('should change length properly', () => {
+    it('should change length properly', function() {
       assert.strictEqual('0001AS99AA60', ulid.encodeTime(1470264322240, 12))
     })
 
-    it('should truncate time if not enough length', () => {
+    it('should truncate time if not enough length', function() {
       assert.strictEqual('AS4Y1E11', ulid.encodeTime(1470118279201, 8))
     })
 
-    describe('should throw an error', () => {
+    describe('should throw an error', function() {
 
-      it('if time greater than (2 ^ 48) - 1', () => {
-        assert.throws(() => ulid.encodeTime(Math.pow(2, 48), 8), Error)
+      it('if time greater than (2 ^ 48) - 1', function() {
+        assert.throws(function() ulid.encodeTime(Math.pow(2, 48), 8), Error)
       })
 
-      it('if time is not a number', () => {
-        assert.throws(() => ulid.encodeTime('test'), Error)
+      it('if time is not a number', function() {
+        assert.throws(function() ulid.encodeTime('test'), Error)
       })
 
-      it('if time is infinity', () => {
-        assert.throws(() => ulid.encodeTime(Infinity), Error)
+      it('if time is infinity', function() {
+        assert.throws(function() ulid.encodeTime(Infinity), Error)
       })
 
-      it('if time is negative', () => {
-        assert.throws(() => ulid.encodeTime(-1), Error)
+      it('if time is negative', function() {
+        assert.throws(function() ulid.encodeTime(-1), Error)
       })
 
-      it('if time is a float', () => {
-        assert.throws(() => ulid.encodeTime(100.1), Error)
+      it('if time is a float', function() {
+        assert.throws(function() ulid.encodeTime(100.1), Error)
       })
 
     })
 
   })
 
-  describe('encodeRandom', () => {
+  describe('encodeRandom', function() {
 
-    it('should return correct length', () => {
+    it('should return correct length', function() {
       assert.strictEqual(12, ulid.encodeRandom(12).length)
     })
 
   })
 
-  describe('decodeTime', () => {
+  describe('decodeTime', function() {
 
-    it('should return correct timestamp', () => {
+    it('should return correct timestamp', function() {
       var timestamp = Date.now()
       var id = ulid(timestamp)
       assert.strictEqual(timestamp, ulid.decodeTime(id))
     })
 
-    it('should accept the maximum allowed timestamp', () => {
+    it('should accept the maximum allowed timestamp', function() {
       assert.strictEqual(281474976710655, ulid.decodeTime('7ZZZZZZZZZZZZZZZZZZZZZZZZZ'))
     })
 
-    describe('should reject', () => {
+    describe('should reject', function() {
 
-      it('malformed strings of incorrect length', () => {
-        assert.throws(() => ulid.decodeTime('FFFF'), Error)
+      it('malformed strings of incorrect length', function() {
+        assert.throws(function() ulid.decodeTime('FFFF'), Error)
       })
 
-      it('strings with timestamps that are too high', () => {
-        assert.throws(() => ulid.decodeTime('80000000000000000000000000'), Error)
+      it('strings with timestamps that are too high', function() {
+        assert.throws(function() ulid.decodeTime('80000000000000000000000000'), Error)
       })
 
     })
 
   })
 
-  describe('ulid', () => {
+  describe('ulid', function() {
 
-    it('should return correct length', () => {
+    it('should return correct length', function() {
       assert.strictEqual(26, ulid().length)
     })
 
-    it('should return expected encoded time component result', () => {
+    it('should return expected encoded time component result', function() {
       assert.strictEqual('01ARYZ6S41', ulid(1469918176385).substring(0, 10))
     })
 
   })
 
-  describe('monotonicity', () => {
+  describe('monotonicity', function() {
 
     function prng() {
       return 0.96
     }
 
-    const stubbedUlid = ulid.factory(prng)
+    var stubbedUlid = ulid.factory(prng)
+    var clock
 
-    let clock
-
-    before(() => {
+    before(function() {
         clock = lolex.install({
           now: 1469918176385,
           toFake: ['Date']
         })
     })
 
-    after(() => {
+    after(function() {
         clock.uninstall()
     })
 
-    it('first call', () => {
+    it('first call', function() {
       assert.strictEqual('01ARYZ6S41YYYYYYYYYYYYYYYY', stubbedUlid())
     })
 
-    it('second call', () => {
+    it('second call', function() {
       assert.strictEqual('01ARYZ6S41YYYYYYYYYYYYYYYZ', stubbedUlid())
     })
 
-    it('third call', () => {
+    it('third call', function() {
       assert.strictEqual('01ARYZ6S41YYYYYYYYYYYYYYZ0', stubbedUlid())
     })
 
-    it('fourth call', () => {
+    it('fourth call', function() {
       assert.strictEqual('01ARYZ6S41YYYYYYYYYYYYYYZ1', stubbedUlid())
     })
 
