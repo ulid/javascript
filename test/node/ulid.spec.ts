4737ff35261a2d2bb16abeb9aa62101f26c07f0c
Perry Mitchell <perry@perrymitchell.net>
2025-03-17 20:39:37 +0200

    Add tests

---
+++ test/node/ulid.spec.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "vitest";
+import {
+    monotonicFactory,
+    ulid,
+} from "../../";
+
+const ULID_REXP = /^[0-7][0-9a-hjkmnp-tv-zA-HJKMNP-TV-Z]{25}$/;
+
+describe("monotonicFactory", () => {
+    it("creates a factory", () => {
+        const factory = monotonicFactory();
+        expect(factory).toBeTypeOf("function");
+    });
+
+    describe("returned factory", () => {
+        it("generates a ULID", () => {
+            const factory = monotonicFactory();
+            const id = factory();
+            expect(id).toMatch(ULID_REXP);
+        });
+    });
+});
+
+describe("ulid", () => {
+    it("generates a ULID", () => {
+        const id = ulid();
+        expect(id).toMatch(ULID_REXP);
+    });
+});
