f7f8231d82b4e7a121a60321b6726a11a6a0a2db
Perry Mitchell <perry@perrymitchell.net>
2025-03-20 18:29:40 +0200

    Add base32 tests

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,6 @@
 export { isValid, monotonicFactory, ulid } from "./ulid.js";
 export { ulidToUUID, uuidToULID } from "./uuid.js";
-export { fixULIDBase32 } from "./crockford.js";
+export { fixULIDBase32, incrementBase32 } from "./crockford.js";
 export { ULIDError, ULIDErrorCode } from "./error.js";
 export { MAX_ULID, MIN_ULID } from "./constants.js";
 export * from "./types.js";
