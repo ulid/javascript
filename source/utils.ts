b17312d2d6de67165624ee68439661ed685e4a3b
Perry Mitchell <perry@perrymitchell.net>
2025-06-08 21:01:07 +0300

    Fix formatting

--- source/utils.ts
+++ source/utils.ts
@@ -3,9 +3,9 @@ import { PRNG } from "./types.js";
 
 export function randomChar(prng: PRNG): string {
     // Currently PRNGs generate fractions from 0 to _less than_ 1, so no "%" is necessary.
-    // However, just in case a future PRNG can generate 1, 
+    // However, just in case a future PRNG can generate 1,
     // we are applying "% ENCODING LEN" to wrap back to the first character
-    const randomPosition = Math.floor(prng() * ENCODING_LEN) % ENCODING_LEN; 
+    const randomPosition = Math.floor(prng() * ENCODING_LEN) % ENCODING_LEN;
     return ENCODING.charAt(randomPosition);
 }
 
